7d182caa9e1ee83f5b65b0ef57604f9d78fd17a7
Andrea Manna <andy97.am@gmail.com>
2023-09-29 01:30:25 +0200

    esame

--- src/data/navLinks.js
+++ src/data/navLinks.js
@@ -5,6 +5,12 @@ export const navLinks = [
     label: 'Home',
     url: 'https://foyr.com/learn/wp-content/uploads/2021/08/design-your-dream-home.jpg',
   },
-  { label: 'About', url: '#' },
-  { label: 'Browse', url: '#' },
+  {
+    label: 'About',
+    url: 'https://iqboatlifts.com/wp-content/uploads/2018/06/Yacht-vs-Boat-Whats-the-Difference-Between-the-Two.jpg',
+  },
+  {
+    label: 'Browse',
+    url: 'https://upload.wikimedia.org/wikipedia/it/thumb/f/f1/BowserSSBU.png/1200px-BowserSSBU.png',
+  },
 ];
